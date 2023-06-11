f7fd3d86ba7dc2caa9dad6abc4d0662e94e69658
Trye <ltrha.hh@gmail.com>
2023-06-11 23:17:42 +0700

    Fix save list

--- src/app/profile/save/page.js
+++ src/app/profile/save/page.js
@@ -1,35 +1,46 @@
+"use client";
 import Image from "next/image";
 import Cookies from "js-cookie";
-export default async function SaveList({ id }) {
-  const res = await getList();
-  const list = res.doc.film;
-  console.log(list);
+import { useState, useEffect } from "react";
+export default function SaveList({ id }) {
+  const [list, setList] = useState(null);
+  useEffect(() => {
+    async function getList() {
+      const res = await fetchList();
+      if (res.status === "fail") return;
+
+      setList(res.doc.film);
+      console.log(list);
+    }
+    getList();
+  }, []);
   return (
     <>
       <section className="profile-save-container">
-        {list.map((el) => {
-          return (
-            <div className="save-element">
-              <div className="save-image">
-                <Image href={el.poster} />
+        {list &&
+          list.map((el) => {
+            return (
+              <div className="save-element">
+                <div className="save-image">
+                  <Image href={el.poster} />
+                </div>
+                <div className="save-description">{el.name}</div>
               </div>
-              <div className="save-description">{el.name}</div>
-            </div>
-          );
-        })}
+            );
+          })}
       </section>
     </>
   );
 }
 
-async function getList() {
+async function fetchList() {
   const list = await fetch(
     "https://movieflix-production.up.railway.app/api/v1/save/mySave",
     {
       headers:
         process.env.NODE_ENV === "development"
           ? {
-              Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NmMyZTQ3MWJiMWY3NDBkMDQyMWU0MSIsImlhdCI6MTY4NjQ5NTQ4MSwiZXhwIjoxNjkxNjc5NDgxfQ.EOKNcfJ9ebl0WymeRlZTtqni50VX941Grie_Abyn62I"}`,
+              Authorization: `Bearer ${Cookies.get("jwt")}`,
             }
           : {},
       cache: "no-cache",
