d2611601e7e124c315bc1be7247908481c5b1a82
tryegg <haltr.hh@gmail.com>
2023-09-07 13:51:28 +0700

    Update requestSearch.js

--- src/app/global-utils/requestSearch.js
+++ src/app/global-utils/requestSearch.js
@@ -2,7 +2,7 @@ export default async function requestSearch(input) {
   const params = input.value;
 
   const searchRes = await fetch(
-    `https://movieflix-production.up.railway.app/api/v1/film/search?search=${params}`,
+    `https://movieflix-ljqx.onrender.com/api/v1/film/search?search=${params}`,
     {
       method: "GET",
     }
