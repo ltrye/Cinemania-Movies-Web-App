f33cbca2a9466ea4ec1a4bd557fbefc8ead5e5dc
tryegg <haltr.hh@gmail.com>
2023-09-07 18:06:34 +0700

    Backend route change

--- src/app/global-utils/GetFilm.js
+++ src/app/global-utils/GetFilm.js
@@ -1,6 +1,6 @@
 export default async function getFilmList(params = null) {
   const movieList = await fetch(
-    `https://https://movieflix-ljqx.onrender.com/api/v1/film${
+    `https://movieflix-ljqx.onrender.com/api/v1/film${
       params !== null ? "?" + params : " "
     }`,
     { next: { revalidate: 5 } }
